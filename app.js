645dfca50476c7c474c4f2d59c474476dc62dd4f
Vinesse <vverning@gmail.com>
2017-05-27 21:35:37 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+var turn = "playerOne";
+var container = document.getElementById("container");
+
+function startGame() {
+    turn = "playerOne";
+    for (var i = 0; i < 9; i++) {
+        var box = document.createElement("div");
+        box.classList.add("box");
+        box.classList.add("symbols");
+        box.dataset.clicked = "false";
+        box.onclick = function() {takeTurn(this)};
+        container.appendChild(box);
+        console.log(i);
+    };
+    document.getElementById("turn").innerHTML = "Player One, Begin.";
+}
+
+function switchTurn() {
+    if (turn === "playerOne") {
+        turn = "playerTwo";
+    } else if (turn === "playerTwo") {
+        turn = "playerOne";
+    };
+}
+
+function takeTurn(el) {
+    if (el.dataset.clicked === "false") {
+        if (turn === "playerOne") {
+            document.getElementById("turn").innerHTML = "One";
+            el.innerHTML = "X";
+        } else if (turn === "playerTwo") {
+            document.getElementById("turn").innerHTML = "Two";
+            el.innerHTML = "O";
+        };
+        el.dataset.clicked = "true";
+        switchTurn();
+    }
+}
+
+function reset() {
+    container.innerHTML = "";
+    startGame();
+}
+
+startGame()
+
